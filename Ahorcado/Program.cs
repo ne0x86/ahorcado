b5fde397cb79a70565f7fc68a1c5727b736ef27f
ne0x86 <ne0x86@users.noreply.github.com>
2019-01-21 14:39:57 +0100

    asd

--- Ahorcado/Program.cs
+++ Ahorcado/Program.cs
@@ -8,8 +8,24 @@ namespace Ahorcado
 {
     class Program
     {
+        static char[] GeneraPalabra(string[] array1)
+        {
+            string palabra;
+            Random aleatorio = new Random();
+            palabra = array1[aleatorio.Next(0, array1.Length)];
+            char[] tablero = new char[palabra.Length];
+            tablero = palabra.ToCharArray();
+            return tablero;
+        }
         static void Main(string[] args)
         {
+            string[] palabras = new string[] { "cosa", "moco", "pedo" };
+            char[] tablero = GeneraPalabra(palabras);
+            foreach (char ele in tablero)
+            {
+                Console.Write(ele+ " ");
+            }
+            Console.ReadKey();
         }
     }
 }
